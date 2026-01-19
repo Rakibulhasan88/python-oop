262e765d71213ea89e73fc5503a3dd48e1c4f9d5
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-01-19 11:12:10 +0600

    Composition Update

--- association_aggregation_composition.py
+++ association_aggregation_composition.py
@@ -1,5 +1,5 @@
 #Association
-
+'''
 class Laptop:
     def __init__ (self, brand):
         self.brand = brand
@@ -14,11 +14,14 @@ class Student:
         
 lp1 = Laptop("Asus")
 student = Student("Rakib",lp1)
-print(student.show_laptop_info())
+student.show_laptop_info()
+
+'''
+
 
 # Aggregation : has a relationship
 # A university has departments.
-
+'''
 class Department:
     def __init__(self, name):
         self.name = name
@@ -40,3 +43,25 @@ dep2 = Department("Business Administration")
 uni1.add_departments(dep1)
 uni1.add_departments(dep2)
 print(uni1.show_departments())
+
+'''
+
+
+# Composition : part of relationship
+# A car has an engine.
+# Every class must be defined inside the other class.
+
+class Engine:
+    def __init__(self, power):
+        self.power = power
+
+class Car:
+    def __init__ (self, model,power):
+        self.model = model
+        self.engine = Engine(power)
+        
+    def show_details(self):
+        print(f"{self.model}has an enginre with {self.engine.power}HP")
+        
+car = Car("Toyota",100)
+car.show_details()
