04f536de56e9f5d804cb2b3829fc41e32bdb0b93
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-01-19 13:09:58 +0600

    Update Multiple and Multilevel inheritance

--- inheritance.py
+++ inheritance.py
@@ -17,7 +17,9 @@ print(f1.first_name)
 print(gf1.first_name)
 '''
 
-# Multiple Inheritance
+# Multilevel Inheritance
+
+'''
 class GrandFather:
     def __init__ (self, color, first_name):
         self.color = color
@@ -37,6 +39,42 @@ class Father(GrandFather):
 class Children(Father, GrandFather):
     def __init__(self, feshion , hobby, color, first_name):
         super().__init__(hobby, color, first_name)
+        self.feshion = feshion
+        
+    def children_method(self):
+        print("I am from Children class")
+
+# gf1 = GrandFather("White","Rakib")
+# f1 = Father("Football", "Red", "Hasan")
+# print(f1.first_name)
+# print(gf1.first_name)
+
+c1 = Children("Modern", "Cricket", "Black", "Khan")
+c1.father_method()
+print(c1.first_name)
+
+'''
+
+# Multiple Inheritance
+class GrandFather:
+    def __init__ (self, color, first_name):
+        self.color = color
+        self.first_name = first_name
+        
+    def gf_method(self):
+        print("I am from GrandFather class")
+        
+class Father(GrandFather):
+    def __init__ (self, hobby):
+        self.hobby = hobby
+        
+    def father_method(self):
+        print("I am from Father class")
+        
+class Children(Father, GrandFather):
+    def __init__(self, feshion , hobby, color, first_name):
+        Father.__init__(self, hobby)
+        GrandFather.__init__(self, color, first_name)
         self.feshion = feshion
         
     def children_method(self):
