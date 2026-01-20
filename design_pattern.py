cf2b8a6c4fe9ac9e04a4a75f6db145987abe64dc
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-01-20 10:47:17 +0600

    Singleton

---
+++ design_pattern.py
@@ -0,0 +1,12 @@
+class Singleton:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+ob1 = Singleton()
+ob2 = Singleton()
+
+print(ob1 is ob2)  # True
