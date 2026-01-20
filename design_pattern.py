5ad044563a1231c5ee3698ffda48c02a651a4a96
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-01-20 10:57:38 +0600

    Builder Design Pattern

--- design_pattern.py
+++ design_pattern.py
@@ -1,12 +1,33 @@
-class Singleton:
-    _instance = None
+# class Singleton:
+#     _instance = None
     
-    def __new__(cls):
-        if cls._instance is None:
-            cls.instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+#     def __new__(cls):
+#         if cls._instance is None:
+#             cls.instance = super(Singleton, cls).__new__(cls)
+#         return cls._instance
 
-ob1 = Singleton()
-ob2 = Singleton()
+# ob1 = Singleton()
+# ob2 = Singleton()
 
-print(ob1 is ob2)  # True
+# print(ob1 is ob2)  # True
+
+# Factory Method Pattern
+class Car:
+    def driver(self):
+        return "Driving a car"
+class Bike:
+    def driver(self):
+        return "Riding a bike"
+class VehicleFactory:
+    @staticmethod
+    def get_vehicle(type):
+        if type == "car":
+            return Car()
+        elif type == "bike":
+            return Bike()
+        else:
+            return ValueError("Unknown vehicle type")
+
+vehicle = VehicleFactory.get_vehicle("bike")
+print(vehicle.driver())
+    
