c9f80cb2efb862e56a2ef1ddf9204dee408de350
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-01-20 11:33:01 +0600

    Builder Design Pattern

--- design_pattern.py
+++ design_pattern.py
@@ -11,6 +11,7 @@
 
 # print(ob1 is ob2)  # True
 
+'''
 # Factory Method Pattern
 class Car:
     def driver(self):
@@ -30,4 +31,39 @@ class VehicleFactory:
 
 vehicle = VehicleFactory.get_vehicle("bike")
 print(vehicle.driver())
+'''
+# Builder Pattern
+
+class Computer:
+    def __init__(self, cpu, ram, storage):
+        self.cpu = cpu
+        self.ram = ram
+        self.storage = storage
+    def __str__(self):
+        return f"Computer with {self.cpu} CPU, {self.ram} RAM, {self.storage} STORAGE."
+
+class ComputerBuilder:
+    def __init__(self):
+        self.cpu = None
+        self.ram = None
+        self.storage = None
+        
+    def set_cpu(self, cpu):
+        self.cpu = cpu
+        return self
+    
+    def set_ram(self, ram):
+        self.ram = ram
+        return self
+        
+    def set_storage(self, storage):
+        self.storage = storage
+        return self
+    
+    def build(self):
+        return Computer(self.cpu, self.ram, self.storage)
+    
+builder = ComputerBuilder()
+computer = builder.set_cpu("intel i9").set_ram("32GB").set_storage("1TB SSD").build()
+print(computer)
     
